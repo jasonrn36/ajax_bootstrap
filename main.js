ef8876f8023660c7c46aabe7c6bcbd3bbc88e1e1
jsonrn36 <jason.rn36@gmail.com>
2026-03-03 19:04:08 -0400

    instalado novamente o setTimeout

--- main.js
+++ main.js
@@ -30,11 +30,10 @@
                 $('#endereco').val(endereco);
 
 
-
+                setTimeout(function(){ //StartSettimeout
                     $(botao).find('i').removeClass('d-none');
                     $(botao).find('span').addClass('d-none');
-
-                    
+                }, 1500) //End setTimeout
 
             })
 
